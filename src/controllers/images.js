a7e3c041b69b2dc5be2efc0076f5f07604cde9c1
Glendon Cheney <glendon.cheney@gmail.com>
2016-06-29 10:21:03 -0500

    set up view template

--- src/controllers/images.js
+++ src/controllers/images.js
@@ -1,9 +1,16 @@
 /* GET '/images/view' */
 module.exports.view = function(req, res) {
-    res.render('images/view', { title: 'View Image' });
+    res.render('images/view', { 
+        title: 'View Image',
+        image: {
+            url: "http://i.imgur.com/qK42fUu.jpg",
+            title: "Golden Gate",
+            description: "The Golden Gate Bridge at night"
+        }
+    });
 };
 
 /* GET '/images/add' */
 module.exports.add = function(req, res) {
-    res.render('images/new', { title: 'Add a New Image' });
+    res.render('images/new', { title: 'Add a New Image'});
 };
