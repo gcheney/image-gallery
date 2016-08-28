cbb94a4dbed292a0a1b56f232d9c9561a7000dd6
Glendon Cheney <glendon.cheney@gmail.com>
2016-08-28 09:17:12 -0500

    Set up basic responses for image api

--- api/controllers/images.js
+++ api/controllers/images.js
@@ -1,9 +1,27 @@
-module.exports.imagesListAll = function (req, res) { };
+var mongoose = require('mongoose');
+var Image = mongoose.model('Image');
 
-module.exports.imagesCreate = function (req, res) { };
+module.exports.imagesListAll = function (req, res) { 
+    sendJsonResponse(res, 200, {"status", "sucess"});
+};
 
-module.exports.imagesReadOne = function (req, res) { };
+module.exports.imagesCreate = function (req, res) { 
+    sendJsonResponse(res, 200, {"status", "sucess"});
+};
 
-module.exports.imagesUpdateOne = function (req, res) { };
+module.exports.imagesReadOne = function (req, res) { 
+    sendJsonResponse(res, 200, {"status", "sucess"});
+};
 
-module.exports.imagesDeleteOne = function (req, res) { };
+module.exports.imagesUpdateOne = function (req, res) { 
+    sendJsonResponse(res, 200, {"status", "sucess"});
+};
+
+module.exports.imagesDeleteOne = function (req, res) { 
+    sendJsonResponse(res, 200, {"status", "sucess"});
+};
+
+var sendJsonResponse = function(res, status, content) {
+    res.status(status);
+    res.json(content);
+};
