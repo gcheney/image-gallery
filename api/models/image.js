29c9acf8760148d4a0463830f553e735a6300e46
Glendon Cheney <glendon.cheney@gmail.com>
2016-08-28 12:26:26 -0500

    Update image model view

--- api/models/image.js
+++ api/models/image.js
@@ -3,7 +3,10 @@ var mongoose = require('mongoose');
 var imageSchema = new mongoose.Schema({
     url: { type: String, required: true },
     title: { type: String, required: true },
+    lead: { type: String },
     description: { type: String },
+    creator: { type: String },
+    date : { type: Date },
     isPublic: { type: Boolean, required: true } 
 });
 
