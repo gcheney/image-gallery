50bfe8d1ef38f65585f93b084ab06ac99c636c32
Glendon Cheney <glendon.cheney@gmail.com>
2016-08-31 15:16:46 -0500

    Add sample data to image view controller

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -3,22 +3,24 @@ var Image = require('../../api/models/image');
 
 /* GET '/images/view' */
 module.exports.view = function(req, res) {
-    var query = {'title': 'Golden Gate'};
+    var image = { 
+            url: "http://i.imgur.com/qK42fUu.jpg", 
+            title: "Golden Gate", 
+            description: "A nice picture of the Golden Gate Bridge", 
+            likes: 12,
+            creator: 'Homer',
+            comments: [
+                {
+                    author: 'Homer',
+                    content: 'I liked this!'
+                }
+            ]
+        }
     
-    Image
-        .find(query)
-        .exec(function(err, image){
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(image);
-                res.render('image/view', { 
-                    title: 'View Image',
-                    image: image
-                });
-            }
+        res.render('image/view', { 
+            title: 'View Image',
+            image: image
         });
-    
 };
 
 /* GET '/images/add' */
