98e34f19a29eff34bc273ea3b7c24bb25cc02655
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-04 15:34:20 -0500

    Add like button effect

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,13 +7,20 @@ $(document).ready(function(){
         $(this).removeClass('transition');
     });
     
+    // like button change
+    $('#btn-like').on('click', function(e){
+        $(this).toggleClass('btn-default');
+        $(this).toggleClass('btn-success');
+    });
+    
     // format the date for image
-    var date = new Date($('#date').text());
+    var datetext = $('#date').text();
+    var date = new Date(datetext);
     var monthNames = [ "January", "February", "March", "April", "May", "June",
                       "July", "August", "September", "October", "November", "December" ];
     var d = date.getDate();
     var m = monthNames[date.getMonth()];
     var y = date.getFullYear();
-    var output = d + ' ' + m + ' ' + y;
-    $('#date').text(output);
+    var formattedDate = d + ' ' + m + ' ' + y;
+    $('#date').text(formattedDate);
 });
