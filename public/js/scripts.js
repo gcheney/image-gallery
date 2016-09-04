76c64b8cee4636589c0c5f9aa685c74b4b73b3b1
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-04 15:17:20 -0500

    Format date string output

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,4 +7,13 @@ $(document).ready(function(){
         $(this).removeClass('transition');
     });
     
+    // format the date for image
+    var date = new Date($('#date').text());
+    var monthNames = [ "January", "February", "March", "April", "May", "June",
+                      "July", "August", "September", "October", "November", "December" ];
+    var d = date.getDate();
+    var m = monthNames[date.getMonth()];
+    var y = date.getFullYear();
+    var output = d + ' ' + m + ' ' + y;
+    $('#date').text(output);
 });
