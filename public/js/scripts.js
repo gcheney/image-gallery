ace2fe1aaa8881b568d3127e812c846f828152b4
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-04 16:38:10 -0500

    Set up lightbox feature for image details

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,26 +1,71 @@
-$(document).ready(function(){
+/*
+ * Custom scripts for image-gallery
+ */
+
+(function($, W, D)
+{ 
+    var JQUERY = {};
+
+    JQUERY.UTIL =
+    {
+        imgLinkHover : function()
+        {
+            //add image transition on hover
+            $('.img-hover').hover(function() {
+                $(this).addClass('transition');  
+            }, function() {
+                $(this).removeClass('transition');
+            });
+        },
+        toggleLikeBtn: function() 
+        {
+            // like button change
+            $('#btn-like').on('click', function(e){
+                $(this).toggleClass('btn-default');
+                $(this).toggleClass('btn-success');
+            });
+        },
+        formatImageDate: function() 
+        {
+            // format the date for image details page
+            var datetext = $('#date').text();
+            var date = new Date(datetext);
+            
+            var monthNames = [ "January", "February", "March", "April", "May", "June",
+                              "July", "August", "September", "October", "November", "December" ];
+            var d = date.getDate();
+            var m = monthNames[date.getMonth()];
+            var y = date.getFullYear();
+            
+            var formattedDate = d + ' ' + m + ' ' + y;
+            $('#date').text(formattedDate);
+        },
+        setupLightbox: function() 
+        {
+            // hide lightbox initalliy
+            $('#lightbox').hide();
     
-    //add image transition on hover
-    $('.img-hover').hover(function() {
-        $(this).addClass('transition');  
-    }, function() {
-        $(this).removeClass('transition');
+            $('.lightbox-trigger').click(function(e) {
+                e.preventDefault();
+
+                //Get img link href for lightbox
+                var href = $(this).attr("href");
+                $('#lightbox-content').html('<img src="' + href + '" />');
+                $('#lightbox').show('slow');	
+            });
+
+            //Click anywhere on the page to get rid of lightbox window
+            $('#lightbox').on('click', function() { 
+                $('#lightbox').hide('fast');
+            });
+        }
+    };
+        
+    $(D).ready(function($) {
+        JQUERY.UTIL.imgLinkHover();
+        JQUERY.UTIL.toggleLikeBtn();
+        JQUERY.UTIL.formatImageDate();
+        JQUERY.UTIL.setupLightbox();
     });
     
-    // like button change
-    $('#btn-like').on('click', function(e){
-        $(this).toggleClass('btn-default');
-        $(this).toggleClass('btn-success');
-    });
-    
-    // format the date for image
-    var datetext = $('#date').text();
-    var date = new Date(datetext);
-    var monthNames = [ "January", "February", "March", "April", "May", "June",
-                      "July", "August", "September", "October", "November", "December" ];
-    var d = date.getDate();
-    var m = monthNames[date.getMonth()];
-    var y = date.getFullYear();
-    var formattedDate = d + ' ' + m + ' ' + y;
-    $('#date').text(formattedDate);
-});
+})(jQuery, window, document);
