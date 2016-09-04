ff011f2c18cd16d8b9f7f8e275a45bd7e59679fc
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-03 21:12:25 -0500

    Setup home route in app to call into api with request

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,13 +1,47 @@
-var mongoose = require('mongoose');
-var Image = require('../../api/models/image');
+var request = require('request');
+
+var apiOptions = {
+    server : "http://localhost:3000"
+};
+
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "";
+}
+
+
+var renderHomepage = function(req, res, responseBody){
+    var message;
+    if (!(responseBody instanceof Array)) {
+        message = "API lookup error";
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = "No images found";
+        }
+    }
+    res.render('home', {
+        title: 'The Image Gallery',
+        pageHeader: {
+            title: 'The Image Gallery',
+        },
+        images: responseBody,
+        message: message
+    });
+};
 
 /* GET '/' */
 module.exports.index = function(req, res) {
-    Image.find({}, function(err, images){
-        res.render('home', { 
-            title: 'Home',
-            images: images
-        });
+    var requestOptions, path;
+    path = '/api/images';
+    
+    requestOptions = {
+        url : apiOptions.server + path,
+        method : "GET",
+        json : {}
+    };
+    
+    request(requestOptions, function(err, response, body) {
+        renderHomepage(req, res, body);
     });
 };
 
