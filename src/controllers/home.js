8988d8557708329f443b0803fdfb7b7a1559713d
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-07 10:32:03 -0500

    Add angular to the application

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -5,43 +5,19 @@ if (process.env.NODE_ENV === 'production') {
     server = "https://image-gallery1.herokuapp.com/";
 }
 
-var renderHomepage = function(req, res, images){
-    var message;
-    if (!(images instanceof Array)) {
-        message = "API lookup error";
-        images = [];
-    } else {
-        if (!images.length) {
-            message = "No images found";
-        }
-    }
+var renderHomepage = function(req, res){
     res.render('home', {
         title: 'Home',
         pageHeader: {
             title: 'The Image Gallery',
             tagline: 'A collection of beautiful images'
-        },
-        images: images,
-        message: message
+        }
     });
 };
 
 /* GET '/' */
 module.exports.index = function(req, res) {
-    var path = '/api/images';
-    
-    var requestOptions = {
-        url : server + path,
-        method : "GET",
-        json : {}
-    };
-    
-    request(requestOptions, function(err, response, data) {
-        if (err) {
-            console.log(err);
-        }
-        renderHomepage(req, res, data);
-    });
+    renderHomepage(req, res);
 };
 
 /* GET '/about' */
