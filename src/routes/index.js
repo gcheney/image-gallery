069b84bd453ea6f698f7ff25783db5dbf177cdbb
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-09 19:03:51 -0500

    Adding Angular routing to application

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 var homeController = require('../controllers/home');
 var imageController = require('../controllers/image');
-var accountController = require('../controllers/account')
+var accountController = require('../controllers/account');
+var angularController = require('../controllers/angular');
 
 
 /* Home */
-router.get('/', homeController.index);
+router.get('/', angularController.index);
 router.get('/about', homeController.about);
 router.get('/contact', homeController.contact);
 
