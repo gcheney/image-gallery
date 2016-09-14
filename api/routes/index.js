0fa4aa42261119f218f89352d4a5395fdb8adc3d
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-14 11:20:32 -0500

    secure api endpoints with jwt auth

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('express-jwt');
 var imageController = require('../controllers/images');
 var commentController = require('../controllers/comments');
 var authController = require('../controllers/auth');
 
+var auth = jwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'payload'
+});
 
 // image api routes
 router.get('/images', imageController.imagesListAll);
@@ -13,10 +18,10 @@ router.put('/images/:imageid', imageController.imagesUpdateOne);
 router.delete('/images/:imageid', imageController.imagesDeleteOne);
 
 // comment api routes
-router.post('/images/:imageid/comments', commentController.commentsCreate);
+router.post('/images/:imageid/comments', auth, commentController.commentsCreate);
 router.get('/images/:imageid/comments/:commentid', commentController.commentsReadOne);
-router.put('/images/:imageid/comments/:commentid', commentController.commentsUpdateOne);
-router.delete('/images/:imageid/comments/:commentid', commentController.commentsDeleteOne);
+router.put('/images/:imageid/comments/:commentid', auth, commentController.commentsUpdateOne);
+router.delete('/images/:imageid/comments/:commentid', auth, commentController.commentsDeleteOne);
 
 // auth api routes
 router.post('/register', authController.register);
