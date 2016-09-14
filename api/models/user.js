94c0fc474af601801c268dec833ca86c9b7b6118
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-14 09:04:37 -0500

    add jwt for user authentication

--- api/models/user.js
+++ api/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require( 'mongoose' );
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var userSchema = new mongoose.Schema({
     email: {
@@ -15,15 +16,30 @@ var userSchema = new mongoose.Schema({
     salt: { type: String }
 });
 
-userSchema.methods.setPassword = function (password) {
+userSchema.methods.setPassword = function(password) {
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64)
         .toString('hex');
 };
 
-userSchema.methods.validatePassword = function (password) {
+userSchema.methods.validatePassword = function(password) {
     var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64)
         .toString('hex');
     
     return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+    // set expiry for 7 days
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+    
+    var payload = {
+        _id: this._id,
+        email: this.email,
+        mame: this.name,
+        exp: parseInt(expiry.getTime()/1000)
+    };
+    
+    return jwt.sign(payload, 'serversecret');
 };
