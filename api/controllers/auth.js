ca30aed4ba548b268edfd2dd81790e523d128002
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-15 08:51:07 -0500

    Fix error with misnamed function

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,7 +4,7 @@ var User = mongoose.model('User');
 
 module.exports.register = function(req, res) {
     if (!req.body.username || !req.body.password) {
-        sendJSONresponse(res, 400, {
+        sendJsonResponse(res, 400, {
             "message": "All fields are required"
         });
         return;
@@ -16,10 +16,10 @@ module.exports.register = function(req, res) {
     
     user.save(function(err) {
         if (err) {
-            sendJSONresponse(res, 404, err);
+            sendJsonResponse(res, 404, err);
         } else {
             var token = user.generateJwt();
-            sendJSONresponse(res, 200, {
+            sendJsonResponse(res, 200, {
                 "token" : token
             });
         }
@@ -28,7 +28,7 @@ module.exports.register = function(req, res) {
 
 module.exports.login = function(req, res) {
     if (!req.body.username || !req.body.password) {
-        sendJSONresponse(res, 400, {
+        sendJsonResponse(res, 400, {
             "message": "All fields are required"
         });
         return;
@@ -36,13 +36,13 @@ module.exports.login = function(req, res) {
     
     passport.authenticate('local', function(err, user, info){
         if (err) {
-            sendJSONresponse(res, 404, err);
+            sendJsonResponse(res, 404, err);
             return;
         }
         
         if (user) {
             var token = user.generateJwt();
-            sendJSONresponse(res, 200, {
+            sendJsonResponse(res, 200, {
                 "token" : token
             });
         } else {
@@ -52,7 +52,7 @@ module.exports.login = function(req, res) {
 };
 
 
-var sendJSONresponse = function(res, status, content) {
+var sendJsonResponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 };
