074978b4f0a1ebc89e06ecb71f6bbf54cc3626f5
Glendon Cheney <glendon.cheney@gmail.com>
2016-09-29 22:18:06 -0500

    Add links to the about page

--- client/about/about.controller.js
+++ client/about/about.controller.js
@@ -10,7 +10,7 @@
         };
         
         vm.main = {
-            content: 'The Image Gallery was created by Glendon Cheney as a place for people to upload their favorite images.\n\n It was created using the MEAN stack, is open source and maintained on <a href="https://github.com/gcheney/image-gallery">Github.</a>'
+            content: 'The Image Gallery was created by <a href="https://glendoncheney.com" target="_blank">Glendon Cheney</a> as a place for people to upload their favorite images.\n\n It was created using the MEAN stack, is open source and maintained on <a href="https://github.com/gcheney/image-gallery" target="_blank">Github.</a>'
         };
     }
 })();
