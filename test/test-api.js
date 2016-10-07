525596596e7dc59d6b772a6a2eb2730d2cf3c795
Glendon Cheney <glendon.cheney@gmail.com>
2016-10-07 17:27:42 -0500

    Add test for /images GET request

--- test/test-api.js
+++ test/test-api.js
@@ -6,7 +6,16 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Images', function() {
-    it('should list ALL images on /images GET');
+    it('should list ALL images on /images GET', function(done) {
+        chai.request(app)
+            .get('/api/images')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                done();
+            });
+    });
     it('should list a SINGLE image on /images/:id GET');
     it('should add a SINGLE image on /images POST');
     it('should update a SINGLE image on /images/:id PUT');
