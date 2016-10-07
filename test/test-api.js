875ba24751ab165d604e3baaaf9914546ffa23af
Glendon Cheney <glendon.cheney@gmail.com>
2016-10-07 17:45:20 -0500

    Update test to use test database

--- test/test-api.js
+++ test/test-api.js
@@ -1,15 +1,43 @@
+process.env.NODE_ENV = 'test';
+
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var app = require('../app');
-var should = chai.should();
+var mongoose = require('mongoose');
 
+var app = require('../app');
+var Image = require('../api/models/image');
+
+var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Images', function() {
+    
+    Image.collection.drop();
+
+    beforeEach(function(done) {
+        var newImage = new Image({
+            title: 'Great Image',
+            description: 'The best image',
+            creator: 'Me',
+            url: 'http://cdn2-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-8.jpg'
+        });
+        newImage.save(function(err) {
+            done();
+        });   
+    });
+    
+    afterEach(function(done) {
+        Image.collection.drop();
+        done();
+    });
+    
     it('should list ALL images on /images GET', function(done) {
         chai.request(app)
             .get('/api/images')
             .end(function(err, res) {
+                if (err) {
+                    console.log(err);
+                }
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('array');
